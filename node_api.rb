81b7181004ffcdce39e61682e43108f127f8f335
mariammal88 <mariammal.moorthi@gmail.com>
2022-10-13 17:29:51 -0700

    Added Comments

--- node_api.rb
+++ node_api.rb
@@ -10,12 +10,15 @@ class NodeApi
 
   def fetch(ids)
     add_nodes(ids)
+    # To join the node_ids if more than one child node present
     response = HTTP.get(BASE_URI + ids.join(','))
     parsed_response = response.parse
 
+    # mapping the child node ids and flattening the array
     child_node_ids = parsed_response.map{|x| x['child_node_ids']}.flatten
     if child_node_ids.length > 0
       add_nodes(child_node_ids)
+      # circle back to the same loop if child node ids present
       fetch(child_node_ids)
     else
       puts "unique_node_ids:"
@@ -30,6 +33,7 @@ class NodeApi
   def add_nodes(ids)
     ids.each do |id|
       if @node_ids.has_key? id
+        # increment the counter for the usage
         @node_ids[id] += 1
       else
         @node_ids[id] = 1
